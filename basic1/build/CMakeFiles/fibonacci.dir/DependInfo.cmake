620fec786a892335cdad9b2d172f9b5d6f65a44f
John Wu <webmaster@leapoahead.com>
2014-02-26 06:47:58 +0800

    basic1: add cmake

---
+++ basic1/build/CMakeFiles/fibonacci.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/john/Desktop/c_review/basic1/fib.c" "/home/john/Desktop/c_review/basic1/build/CMakeFiles/fibonacci.dir/fib.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
