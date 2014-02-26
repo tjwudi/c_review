3e5c2d31393fa5b5e143dedb1ae4994d3b5ac9c3
John Wu <webmaster@leapoahead.com>
2014-02-26 19:05:16 +0800

    check_bits: a demo to show the width of integer type

---
+++ check_bit/build/CMakeFiles/check_bit.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/john/Desktop/c_review/check_bit/check_bit.cpp" "/home/john/Desktop/c_review/check_bit/build/CMakeFiles/check_bit.dir/check_bit.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ check_bit/build/CMakeFiles/check_bit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/check_bit.dir/check_bit.cpp.o"
+  "check_bit.pdb"
+  "check_bit"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/check_bit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
